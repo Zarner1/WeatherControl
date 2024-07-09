b7619bf35dea42dadb9cda5eb716f9f5ecd20db0
Zarner1 <yildirim340458@gmail.com>
2024-07-09 17:42:56 +0300

    düzneleme

--- weather.py
+++ weather.py
@@ -9,7 +9,7 @@ def weather_get():
 
     textbig2 = ctk.CTkTextbox(window, 175, 200, corner_radius=5, bg_color="grey",border_color="yellow",border_width=3,font=("Helvetica",15))
     canvas.create_window(400, 600, window=textbig2)
-    Api_Key = "2606f769271b8d545fe3458b2b72ed9f"
+    Api_Key = "30fe3b577ded8dc1441ad6359b821241"
     final_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(text1.get(), Api_Key)
     result = requests.get(final_URL)
     data = result.json()
@@ -46,24 +46,24 @@ canvas.create_image(400, 400, image=tk_image)
 #istanbul
 canvas.create_rectangle(50, 50, 175, 175, outline="blue", width=5)
 canvas.create_text(110, 190, text="Istanbul", font=("Helvetica", 20), anchor="center", fill="red")
-istanbulURL="http://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=2606f769271b8d545fe3458b2b72ed9f"
+istanbulURL="http://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=30fe3b577ded8dc1441ad6359b821241"
 response1=requests.get(istanbulURL)
 dataIST=response1.json()
 canvas.create_text(110,110,text=f"{round(dataIST['main']['temp']-273.15,2)} °C",font=("Helvetica",22),fill="Red")
 #ankara
 canvas.create_rectangle(350, 50, 475, 175, outline="blue", width=5)
 canvas.create_text(410, 190, text="Ankara", font=("Helvetica", 20), anchor="center", fill="red")
-ankaraURL="http://api.openweathermap.org/data/2.5/weather?q=Ankara&appid=2606f769271b8d545fe3458b2b72ed9f"
+ankaraURL="http://api.openweathermap.org/data/2.5/weather?q=Ankara&appid=30fe3b577ded8dc1441ad6359b821241"
 response2=requests.get(ankaraURL)
 dataANK=response2.json()
 canvas.create_text(410,110,text=f"{round(dataANK['main']['temp']-273.15,2)} °C",font=("Helvetica",22),fill="Red")
 #Izmır
 canvas.create_rectangle(650, 50, 775, 175, outline="blue", width=5)
 canvas.create_text(710, 190, text="Izmir", font=("Helvetica", 20), anchor="center", fill="red")
-izmirURL="http://api.openweathermap.org/data/2.5/weather?q=Izmir&appid=2606f769271b8d545fe3458b2b72ed9f"
+izmirURL="http://api.openweathermap.org/data/2.5/weather?q=Izmir&appid=30fe3b577ded8dc1441ad6359b821241"
 response3=requests.get(izmirURL)
 dataIZM=response3.json()
-canvas.create_text(710,110,text=f"{round(dataIST['main']['temp']-273.15,2)} °C",font=("Helvetica",22),fill="Red")
+canvas.create_text(710,110,text=f"{round(dataIZM['main']['temp']-273.15,2)} °C",font=("Helvetica",22),fill="Red")
 #şehir girme texti
 canvas.create_text(400, 375, text="Şehiri Giriniz", font=("Helvetica", 18), anchor="center", fill="Orange")
 text1 = ctk.CTkEntry(window, corner_radius=1, border_color="orange", border_width=3,
